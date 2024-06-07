09d9a3a5cf307403d4320b402944b41478d19a95
mayurpatil96k <mayur1792001@gmail.com>
2024-06-07 12:23:16 +0530

    [Mayur]Add: Intigrated axios with user service

--- src/components/services/UserService.js
+++ src/components/services/UserService.js
@@ -1,4 +1,10 @@
-export const signup = (signdata)=>{
-console.log("agaya bhai tu service me");
-console.log(signdata);
+import { post } from 'axios'
+
+export const signup = (signdata) => {
+  console.log('agaya bhai tu service me')
+  console.log(signdata);
+  const headers ={
+    'Content-Type': 'application/json'
+  }
+  return post('user/signup',signdata,headers);
 }
